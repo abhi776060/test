0c67972d657d43fb1d23462ac77a677b9395cc8d
abhishek <abhishek.kyathanahalli@modernchipsolutions>
2022-03-04 17:57:05 +0530

    04-03-2022

--- django-projects/harsha/use.py
+++ django-projects/harsha/use.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_doc=requests.get('https://timesofindia.indiatimes.com/rssfeedstopstories.cms').json()
-soup = BeautifulSoup(html_doc, 'html.parser')
+html_doc=requests.get('https://timesofindia.indiatimes.com/rssfeedstopstories.cms')
+soup = BeautifulSoup(html_doc.json(), 'html.parser')
 list1=[]
 for x in soup.soup.title:
     list1.append(x)
-print(list1)
+# print(list1)
